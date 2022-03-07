3295c9558e3632a28f408d41bc6b6443cd3335d8
Abhi <programwithabhi@gmail.com>
2022-03-07 17:18:59 +0530

    Adding the schema and connecting to the database and added the correspinig routes to the parent request

---
+++ models/Notes.js
@@ -0,0 +1,24 @@
+const mongoose = require('mongoose');
+const { Schema } = mongoose;
+
+const notesSchema = new Schema({
+    title: {
+        type: String,
+        required: true,
+    },
+    description: {
+        type: String,
+        required: true
+    },
+    tag: {
+        type: String,
+        default:"General"
+    },
+    date: {
+        type: Date,
+        // Dont the give the like Date.now() that it means default is the Date.now function
+        default: Date.now
+    }
+});
+
+module.exports = mongoose.model('notes',notesSchema)
